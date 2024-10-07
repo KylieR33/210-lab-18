c3dd93192f69d14bd644af716f0c572482f92794
Kylie Roxo <kylieroxo@Kylies-MacBook-Pro.local>
2024-10-07 00:22:28 -0400

    first commit

--- main.cpp
+++ main.cpp
@@ -20,6 +20,48 @@ Node* inserttail(Node* head, float new_value);
 float average(Node* head);
 
 int main(){
-
-cout << "Which linked list method should we use" << endl;
+    int choice;
+    float rate;
+    string comment;
+    char again;
+    Node *head = nullptr;
+    vector<string> word;
+    
+    cout << "Which linked list method should we use" << endl;
     cout <<"  [1] New nodes are added at the head of the linked list" << endl;
+    cout <<"  [2] New nodes are added at the tail of the linked list" << endl;
+    cout << "  Choice: ";
+    cin >> choice;
+
+    if (choice == 2){ 
+    do
+   {
+    cout << "Enter review rating 0-5: ";
+    cin >> rate;
+    head = inserttail(head, rate);
+    cout << "Enter review comments: ";
+    cin >> comment;
+    word.push_back(comment);
+    cout << "Enter another review? Y/N:";
+    cin >> again;
+    } while (again == 'y');
+   }
+   else {
+       cout << "";
+   }
+
+   return 0;
+}
+
+Node* inserttail(Node* head, float new_value) {
+    Node* new_node = new Node(new_value);
+    if (head == nullptr) {
+        return new_node;
+    }
+    Node* last = head;
+    while (last->next != nullptr) {
+        last = last->next;
+    }
+    last->next = new_node;
+    return head;
+}
