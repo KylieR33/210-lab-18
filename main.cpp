cf32da63126512ca43e8c082f02a3a814c9ae337
Kylie Roxo <kylieroxo@Kylies-MacBook-Pro.local>
2024-10-07 00:12:04 -0400

    first commit

--- main.cpp
+++ main.cpp
@@ -14,3 +14,12 @@ class Node {
     next = nullptr;
     }
 };
+
+void printlist(Node* );
+Node* inserttail(Node* head, float new_value);
+float average(Node* head);
+
+int main(){
+
+cout << "Which linked list method should we use" << endl;
+    cout <<"  [1] New nodes are added at the head of the linked list" << endl;
