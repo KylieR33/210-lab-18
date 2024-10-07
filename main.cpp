f39760f6b1c626af92a79f629e6da164cea01d53
Kylie Roxo <kylieroxo@Kylies-MacBook-Pro.local>
2024-10-07 00:37:05 -0400

    first commit

--- main.cpp
+++ main.cpp
@@ -4,28 +4,28 @@
 using namespace std;
 
 class Node {
-    public:
+    public://public variables
     float value;
-    Node *next;
+    Node *next;//node point
     
-    Node(float new_value) {
+    Node(float new_value) {//value = new_value and next points to null
     value = new_value;
     next = nullptr;
     }
 };
 
-void printlist(Node* );
+void printlist(Node* );//function prototypes
 Node* inserttail(Node* head, float new_value);
 float average(Node* head);
 
 int main(){
     int count = 0;
     int total = 0;
-    int choice;
-    float rate;
-    string comment;
-    char again;
-    Node *head = nullptr;
+    int choice;//user input
+    float rate;//user input 
+    string comment;//user input 
+    char again;//user input 
+    Node *head = nullptr;// initilaize head and point to null
     vector<string> word;
     
     cout << "Which linked list method should we use" << endl;
@@ -34,12 +34,12 @@ int main(){
     cout << "  Choice: ";
     cin >> choice;
 
-    if (choice == 2){ 
+    if (choice == 2){ //do while loop to see if user wants to enter input again
     do
    {
     cout << "Enter review rating 0-5: ";
     cin >> rate;
-    head = inserttail(head, rate);
+    head = inserttail(head, rate);//function call
     cout << "Enter review comments: ";
     cin >> comment;
     word.push_back(comment);
@@ -50,32 +50,32 @@ int main(){
    else {
        cout << "";
    }
-   
+
    cout << endl;
    cout << "Outputting all reviews: " << endl;
-   printlist(head);
+   printlist(head);//function call
    for (int i = 0; i < word.size(); i++){
        int count = 1;
         cout <<"Comments #" <<count++ << ": " << word[i] << endl;
    }
-   cout << "Average: " << average(head) << endl;
+   cout << "Average: " << average(head) << endl;//function call
 
    return 0;
 }
 
-Node* inserttail(Node* head, float new_value) {
+Node* inserttail(Node* head, float new_value) {//Function to insert on the end rather than on the head 
     Node* new_node = new Node(new_value);
     if (head == nullptr) {
         return new_node;
     }
-    Node* last = head;
-    while (last->next != nullptr) {
-        last = last->next;
+    Node* end = head;
+    while (end->next != nullptr) {
+        end = end->next;
     }
-    last->next = new_node;
+    end->next = new_node;
     return head;
 }
-void printlist(Node * hd){
+void printlist(Node * hd){//function to print the linked list
     int count = 1;
     Node * current = hd;
     while (current != nullptr) {
@@ -84,7 +84,7 @@ void printlist(Node * hd){
     
     }
 }
-float average(Node* head){
+float average(Node* head){//find the average of the list 
     float sum = 0;
     int add = 0;
     float average = 0.0;
